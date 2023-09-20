3bae7459ba3117feced217c093c5e4bc255db742
Guy Bedford <gbedford@fastly.com>
2023-09-19 17:13:17 -0700

    build fix

--- spidermonkey_embedding/spidermonkey_embedding.cpp
+++ spidermonkey_embedding/spidermonkey_embedding.cpp
@@ -805,6 +805,21 @@ check_init()
   return R.init_err;
 }
 
+const char *core_ty_str(CoreVal ty)
+{
+  switch (ty)
+  {
+  case CoreVal::I32:
+    return "i32";
+  case CoreVal::I64:
+    return "i64";
+  case CoreVal::F32:
+    return "f32";
+  case CoreVal::F64:
+    return "f64";
+  }
+}
+
 bool first_call = true;
 
 __attribute__((export_name("call"))) uint32_t call(uint32_t fn_idx, void *argptr)
@@ -971,19 +986,4 @@ __attribute__((export_name("post_call"))) void post_call(uint32_t fn_idx)
   js::RunJobs(R.cx);
   JS_MaybeGC(R.cx);
   log("(post_call) end");
-}
-
-const char *core_ty_str(CoreVal ty)
-{
-  switch (ty)
-  {
-  case CoreVal::I32:
-    return "i32";
-  case CoreVal::I64:
-    return "i64";
-  case CoreVal::F32:
-    return "f32";
-  case CoreVal::F64:
-    return "f64";
-  }
 }
