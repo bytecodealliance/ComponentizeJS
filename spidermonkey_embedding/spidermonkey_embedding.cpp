aadc6c5951eb15334fdf298099261cddacb93ee3
Guy Bedford <gbedford@fastly.com>
2023-10-13 13:28:46 -0700

    use direct isInt32

--- spidermonkey_embedding/spidermonkey_embedding.cpp
+++ spidermonkey_embedding/spidermonkey_embedding.cpp
@@ -951,10 +951,10 @@ __attribute__((export_name("call"))) uint32_t call(uint32_t fn_idx, void *argptr
       }
       break;
     case CoreVal::F32:
-      *((float *)retptr) = (r.asRawBits() >> 32) == 0xFFFFFF81 ? static_cast<float>(r.toInt32()) : static_cast<float>(r.toDouble());
+      *((float *)retptr) = r.isInt32() ? static_cast<float>(r.toInt32()) : static_cast<float>(r.toDouble());
       break;
     case CoreVal::F64:
-      *((double *)retptr) = (r.asRawBits() >> 32) == 0xFFFFFF81 ? static_cast<double>(r.toInt32()) : r.toDouble();
+      *((double *)retptr) = r.isInt32() ? static_cast<double>(r.toInt32()) : r.toDouble();
       break;
     }
   }
