01be85c31921e649a50fbb4d7bf1be94f48dc91e
Victor Adossi <vadossi@cosmonic.com>
2025-08-01 15:51:16 +0900

    fix(ci): max concurrent suites during weval test

--- test/vitest.ts
+++ test/vitest.ts
@@ -8,6 +8,14 @@ import { defineConfig } from 'vitest/config';
  */
 const TIMEOUT_MS = process.env.CI ? 240_000 : 120_000;
 
+/**
+ * On relatively modest machines in CI when weval is enabled,
+ * tests that use it (i.e. any test that does a build w/ enableAot) can
+ * suffer from CPU resource contention.
+ */
+const MAX_CONCURRENT_SUITES =
+  process.env.CI && process.env.WEVAL_TEST ? 1 : undefined;
+
 const REPORTERS = process.env.GITHUB_ACTIONS
   ? ['verbose', 'github-actions']
   : ['verbose'];
@@ -29,6 +37,16 @@ export default defineConfig({
     retry: RETRY,
     printConsoleTrace: true,
     passWithNoTests: false,
+    /**
+     * We use only one concurrent suite because tools like Weval
+     * are very sensitive to other operations, and during tests
+     * we will blow through timeouts and performance
+     *
+     * Inside individual suites, test.concurrent will still enable tests
+     * to be run side-by-side (see TIMEOUT_MS and RETRY which enable
+     * those tests to eventually pass despite increased load).
+     */
+    maxConcurrentSuites: MAX_CONCURRENT_SUITES,
     include: ['test/**/*.js'],
     setupFiles: ['test/meta-resolve-stub.ts'],
     exclude: [
