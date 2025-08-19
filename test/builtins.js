5a758018f882971b9af766adbe57aac9dd6cc90a
Victor Adossi <123968127+vados-cosmonic@users.noreply.github.com>
2025-08-19 20:14:58 +0900

    refactor(tests): split up test suites, update docs (#278)
    * refactor(tests): split up test suites, update docs
    
    * fix(tests): more careful server instantiation
    
    * refactor(tests): disable concurrent tests for weval runs
    
    * fix(ci): max concurrent suites during weval test

---
+++ test/builtins.js
@@ -0,0 +1,130 @@
+import { readFile, readdir, mkdir, writeFile } from 'node:fs/promises';
+import { spawn } from 'node:child_process';
+import { fileURLToPath, URL } from 'node:url';
+
+import { componentize } from '@bytecodealliance/componentize-js';
+import { transpile } from '@bytecodealliance/jco';
+
+import { suite, test, assert } from 'vitest';
+
+import {
+  DEBUG_TRACING_ENABLED,
+  WEVAL_TEST_ENABLED,
+  DEBUG_TEST_ENABLED,
+  maybeLogging,
+} from './util.js';
+
+suite('Builtins', async () => {
+  const builtins = await readdir(new URL('./builtins', import.meta.url));
+
+  for (const filename of builtins) {
+    const name = filename.slice(0, -3);
+    const testFn = WEVAL_TEST_ENABLED ? test : test.concurrent;
+    testFn(name, async () => {
+      const {
+        source,
+        test: runTest,
+        disableFeatures,
+        enableFeatures,
+      } = await import(`./builtins/${name}.js`);
+
+      const { component } = await componentize(
+        source,
+        `
+        package local:runworld;
+        world runworld {
+          export run: func();
+        }
+      `,
+        {
+          sourceName: `${name}.js`,
+          // also test the debug build while we are about it (unless testing Weval)
+          debugBuild: DEBUG_TEST_ENABLED,
+          enableFeatures,
+          disableFeatures: maybeLogging(disableFeatures),
+          enableAot: WEVAL_TEST_ENABLED,
+        },
+      );
+
+      const { files } = await transpile(component, {
+        name,
+        wasiShim: true,
+        tracing: DEBUG_TRACING_ENABLED,
+      });
+
+      await mkdir(new URL(`./output/${name}/interfaces`, import.meta.url), {
+        recursive: true,
+      });
+
+      await writeFile(
+        new URL(`./output/${name}.component.wasm`, import.meta.url),
+        component,
+      );
+
+      for (const file of Object.keys(files)) {
+        await writeFile(
+          new URL(`./output/${name}/${file}`, import.meta.url),
+          files[file],
+        );
+      }
+
+      await writeFile(
+        new URL(`./output/${name}/run.js`, import.meta.url),
+        `
+        import { run } from './${name}.js';
+        run();
+      `,
+      );
+
+      try {
+        await runTest(async function run() {
+          let stdout = '',
+            stderr = '',
+            timeout;
+          try {
+            await new Promise((resolve, reject) => {
+              const cp = spawn(
+                process.argv[0],
+                [
+                  fileURLToPath(
+                    new URL(`./output/${name}/run.js`, import.meta.url),
+                  ),
+                ],
+                { stdio: 'pipe' },
+              );
+              cp.stdout.on('data', (chunk) => {
+                stdout += chunk;
+              });
+              cp.stderr.on('data', (chunk) => {
+                stderr += chunk;
+              });
+              cp.on('error', reject);
+              cp.on('exit', (code) =>
+                code === 0 ? resolve() : reject(new Error(stderr || stdout)),
+              );
+              timeout = setTimeout(() => {
+                reject(
+                  new Error(
+                    'test timed out with output:\n' +
+                      stdout +
+                      '\n\nstderr:\n' +
+                      stderr,
+                  ),
+                );
+              }, 10_000);
+            });
+          } catch (err) {
+            throw { err, stdout, stderr };
+          } finally {
+            clearTimeout(timeout);
+          }
+
+          return { stdout, stderr };
+        });
+      } catch (err) {
+        if (err.stderr) console.error(err.stderr);
+        throw err.err || err;
+      }
+    });
+  }
+});
--- test/vitest.ts
+++ test/vitest.ts
@@ -1,17 +1,52 @@
 import { defineConfig } from 'vitest/config';
 
-const DEFAULT_TIMEOUT_MS = 120_000;
+/**
+ * Under high concurrency, tests (in partticular bindings generation) can take
+ * much longer than expected.
+ *
+ * This issues primarily happen in CI and not locally on a sufficiently powerful machine.
+ */
+const TIMEOUT_MS = process.env.CI ? 240_000 : 120_000;
+
+/**
+ * On relatively modest machines in CI when weval is enabled,
+ * tests that use it (i.e. any test that does a build w/ enableAot) can
+ * suffer from CPU resource contention.
+ */
+const MAX_CONCURRENT_SUITES =
+  process.env.CI && process.env.WEVAL_TEST ? 1 : undefined;
 
 const REPORTERS = process.env.GITHUB_ACTIONS
   ? ['verbose', 'github-actions']
   : ['verbose'];
 
+/**
+ *
+ * Retry is set because there are issues that can randomly happen under high test concurrency:
+ *   - file systems issues (weval[.exe] is busy)
+ *   - performance under concurrency issues (see `builtins.performance.js` test)
+ *
+ * These issues primarily happen in CI and not locally, on a sufficiently powerful machine.
+ */
+const RETRY = process.env.CI ? 3 : 0;
+
 export default defineConfig({
   test: {
     reporters: REPORTERS,
     disableConsoleIntercept: true,
+    retry: RETRY,
     printConsoleTrace: true,
     passWithNoTests: false,
+    /**
+     * We use only one concurrent suite because tools like Weval
+     * are very sensitive to other operations, and during tests
+     * we will blow through timeouts and performance
+     *
+     * Inside individual suites, test.concurrent will still enable tests
+     * to be run side-by-side (see TIMEOUT_MS and RETRY which enable
+     * those tests to eventually pass despite increased load).
+     */
+    maxConcurrentSuites: MAX_CONCURRENT_SUITES,
     include: ['test/**/*.js'],
     setupFiles: ['test/meta-resolve-stub.ts'],
     exclude: [
@@ -21,8 +56,8 @@ export default defineConfig({
       'test/output/*',
       'test/util.js',
     ],
-    testTimeout: DEFAULT_TIMEOUT_MS,
-    hookTimeout: DEFAULT_TIMEOUT_MS,
-    teardownTimeout: DEFAULT_TIMEOUT_MS,
+    testTimeout: TIMEOUT_MS,
+    hookTimeout: TIMEOUT_MS,
+    teardownTimeout: TIMEOUT_MS,
   },
 });
